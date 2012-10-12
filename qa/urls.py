a3894b5ce1fae8eebc2a3791e44d61d762a81300
Amir Nissim <amirnissim@gmail.com>
2012-10-12 17:50:11 +0200

    voters can post questions when logged in

--- qa/urls.py
+++ qa/urls.py
@@ -3,13 +3,17 @@ from django.conf.urls.defaults import patterns
 urlpatterns = patterns('',
     (r'^$', 'qa.views.home'),
     (r'^/$', 'qa.views.home'),
+
     (r'^q/$', 'qa.views.questions'),
     (r'^q/(?P<q_id>\d+)/$', 'qa.views.view_question'),
-    (r'^add_answer/(?P<q_id>\d+)/$', 'qa.views.add_answer'),
 
     (r'^candidates/$', 'qa.views.candidates'),
     (r'^candidate/(?P<candidate_id>\d+)/$', 'qa.views.view_candidate'),
 
     (r'^voters/$', 'qa.views.voters'),
     (r'^voter/(?P<voter_id>\d+)/$', 'qa.views.view_voter'),
+
+    (r'^add_answer/(?P<q_id>\d+)/$', 'qa.views.add_answer'),
+    (r'^add_question/$', 'qa.views.add_question'),
+
 )
--- qa/views.py
+++ qa/views.py
@@ -4,18 +4,6 @@ from django.template.context import RequestContext
 from qa.models import Question, Answer
 from django.http import HttpResponse, HttpResponseForbidden, HttpResponseBadRequest
 
-def create_question(request):
-    if not request.user.is_authenticated():
-        return HttpResponseForbidden()
-
-    subject = request.POST.get("subject")
-    content = request.POST.get("content")
-
-    q = Question(author=request.user, subject=subject, content=content)
-    q.save()
-
-    return HttpResponse("OK")
-
 def home(request):
     return render(request, "home.html")
 
@@ -38,7 +26,6 @@ def view_voter(request, voter_id):
     voter = get_object_or_404(User, id=voter_id)
     return render_to_response("view_voter.html", locals(), context_instance=RequestContext(request))
 
-
 def questions(request):
     questions = Question.objects.all()
     return render_to_response("questions.html", locals(), context_instance=RequestContext(request))
@@ -47,6 +34,18 @@ def view_question(request, q_id):
     question = get_object_or_404(Question, id=q_id)
     answers = question.answers.all()
     return render_to_response("view_question.html", locals(), context_instance=RequestContext(request))
+
+def add_question(request):
+    if not request.user.is_authenticated():
+        return HttpResponseForbidden("You cannot post questions")
+
+    subject = request.POST.get("subject")
+    content = request.POST.get("content")
+
+    q = Question(author=request.user, subject=subject, content=content)
+    q.save()
+
+    return HttpResponse("OK")
 
 def add_answer(request, q_id):
     if not request.user.is_authenticated(): # TODO: check that user is a candidate (can answer questions)
