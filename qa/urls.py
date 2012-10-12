b48fa1142eec805fe38bc872540400954c7a3ff3
Amir Nissim <amirnissim@gmail.com>
2012-10-12 17:32:35 +0200

    voters and candidates pages skeletons

--- qa/urls.py
+++ qa/urls.py
@@ -2,8 +2,14 @@ from django.conf.urls.defaults import patterns
 
 urlpatterns = patterns('',
     (r'^$', 'qa.views.home'),
-    (r'^q/$', 'qa.views.list_questions'),
+    (r'^/$', 'qa.views.home'),
+    (r'^q/$', 'qa.views.questions'),
     (r'^q/(?P<q_id>\d+)/$', 'qa.views.view_question'),
     (r'^add_answer/(?P<q_id>\d+)/$', 'qa.views.add_answer'),
 
+    (r'^candidates/$', 'qa.views.candidates'),
+    (r'^candidate/(?P<candidate_id>\d+)/$', 'qa.views.view_candidate'),
+
+    (r'^voters/$', 'qa.views.voters'),
+    (r'^voter/(?P<voter_id>\d+)/$', 'qa.views.view_voter'),
 )
--- qa/views.py
+++ qa/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import Group, User
 from django.shortcuts import render, render_to_response, get_object_or_404
 from django.template.context import RequestContext
 from qa.models import Question, Answer
@@ -18,24 +19,44 @@ def create_question(request):
 def home(request):
     return render(request, "home.html")
 
-def list_questions(request):
+def candidates(request):
+    g = Group.objects.get(name="candidates")
+    candidates = g.user_set.all()
+    return render_to_response("candidates.html", locals(), context_instance=RequestContext(request))
+
+def view_candidate(request, candidate_id):
+    candidate = get_object_or_404(User, id=candidate_id)
+    return render_to_response("view_candidate.html", locals(), context_instance=RequestContext(request))
+
+
+def voters(request):
+    g = Group.objects.get(name="voters")
+    voters = g.user_set.all()
+    return render_to_response("voters.html", locals(), context_instance=RequestContext(request))
+
+def view_voter(request, voter_id):
+    voter = get_object_or_404(User, id=voter_id)
+    return render_to_response("view_voter.html", locals(), context_instance=RequestContext(request))
+
+
+def questions(request):
     questions = Question.objects.all()
     return render_to_response("questions.html", locals(), context_instance=RequestContext(request))
 
 def view_question(request, q_id):
     question = get_object_or_404(Question, id=q_id)
     answers = question.answers.all()
-    return render_to_response("question.html", locals(), context_instance=RequestContext(request))
+    return render_to_response("view_question.html", locals(), context_instance=RequestContext(request))
 
 def add_answer(request, q_id):
     if not request.user.is_authenticated(): # TODO: check that user is a candidate (can answer questions)
-        return HttpResponseForbidden()
+        return HttpResponseForbidden("You must be a candidate to add an answer")
 
     question = Question.objects.get(id=q_id)
     content = request.POST.get("content")
 
     if not (question and content):
-        return HttpResponseBadRequest()
+        return HttpResponseBadRequest("Question does not exist, or empty answer")
 
     answer = Answer(author=request.user, content=content, question=question)
     answer.save()
