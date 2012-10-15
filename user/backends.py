076f1da1c3298ff59cf3021025b49e5b5f42e30f
Moshe Kamensky <samvimes@fastmail.fm>
2012-10-15 19:18:04 +0200

    Auth done

--- user/backends.py
+++ user/backends.py
@@ -1,8 +1,18 @@
 from registration.backends.default import DefaultBackend
+from registration import signals
 from user.forms import TwoStepRegForm
 
 class RegBackend(DefaultBackend):
     def get_form_class(self, request):
         return TwoStepRegForm
+
+    def register(self, request, **kwargs):
+        user = super(RegBackend, self).register(request, **kwargs)
+        user.first_name = kwargs['first_name']
+        user.last_name = kwargs['last_name']
+        user.save()
+        return user
+
+
 
 
