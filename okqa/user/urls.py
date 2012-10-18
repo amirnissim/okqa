9f810312873f092f6bd10ac5f4be8d013baf5ff4
Moshe Kamensky <samvimes@fastmail.fm>
2012-10-18 21:14:08 +0200

    Migrated registration/login to new repository, some cleanup

--- okqa/user/urls.py
+++ okqa/user/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
 from registration.views import register
+from okqa.user.views import login
 
 urlpatterns = patterns('',
     url(r'^register/$', 
         register,
-        {'backend': 'user.backends.RegBackend'},
+        {'backend': 'okqa.user.backends.RegBackend'},
         name='registration_register'),
+    url(r'^login/', login, name='auth_login'),
     url(r'', include('registration.backends.default.urls')),
 )
--- okqa/user/views.py
+++ okqa/user/views.py
@@ -1,19 +1,38 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseForbidden, HttpResponseRedirect, HttpResponseBadRequest
-from django.contrib.auth import login, authenticate
-from forms import RegistrationForm
-from django.contrib.auth.models import Group, User
+from django.http import HttpResponse, HttpResponseForbidden, HttpResponseBadRequest, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext as _
+from django import forms
+from django.contrib import messages
 
-def create_user(request):
-    if request.method == 'POST':
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = authenticate(username = form.cleaned_data['username'],
-                                password = form.cleaned_data['password1'])
-            login(request, user)
-            return HttpResponseRedirect('profile')
-    else:
-        form = RegistrationForm()
-    return render(request, 'create_user.html', { 'form': form, })
+def login(request):
+    nxt = request.META['HTTP_REFERER'] if request.META['HTTP_REFERER'] else reverse('home')
+    if request.method == "POST":
+        user = request.user
+        if user.is_authenticated():
+            auth_logout(request)
+        else:
+            username = request.POST['username']
+            password = request.POST['password']
+            if '@' in username:
+                try:
+                    u = User.objects.get(email__iexact=username)
+                    username=u.username
+                except User.DoesNotExist:
+                    messages.warning(request, _('Email not found'))
+                    return HttpResponseRedirect(nxt)
+                except:
+                    messages.warning(request, _('Email lookup error, please file a bug'))
+                    return HttpResponseRedirect(nxt)
+            user = authenticate(username=username, password=password)
+            if user is None:
+                messages.warning(request, _('Incorrect Username/Password'))
+            else:
+                if user.is_active:
+                    auth_login(request, user)
+                else:
+                    messages.warning(request, _('Account is inactive'))
+    return HttpResponseRedirect(nxt)
+
 
