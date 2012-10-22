83daf843101ffa5eab704a03bc2ede1a0647aa6e
Benny Daon <bennydaon@gmail.com>
2012-10-23 00:16:22 +0200

    Fixing #3 : Searching now works from the nav bar
    Need better formatting and a better results page

--- okqa/party/context_processors.py
+++ okqa/party/context_processors.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.forms import AuthenticationForm
-
+from haystack.forms import SearchForm
 
 def forms(request):
-        if not request.user.is_authenticated():
-            return {"login_form": AuthenticationForm}
-        else:
-            return {}
+
+    context = {"search_form": SearchForm()}
+    if not request.user.is_authenticated():
+        context["login_form"] = AuthenticationForm()
+    return context
 
 
