1d3c3cf923e6cbf4bbb22ac522e5164768e9c9bd
Benny Daon <bennydaon@gmail.com>
2012-10-22 22:07:07 +0200

    Added a question form

--- okqa/qa/forms.py
+++ okqa/qa/forms.py
@@ -1,8 +1,13 @@
 from django.forms import ModelForm
-from models import Answer
+from models import Answer, Question
 
 class AnswerForm(ModelForm):
     class Meta:
         model = Answer
         fields = ("content",)
+
+class QuestionForm(ModelForm):
+    class Meta:
+        model = Question
+        fields = ("subject", "content", "tags", )
 
