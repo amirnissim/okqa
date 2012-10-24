ce7726d11b49f0a6ccda249a72345ae2a5d1b565
Benny Daon <bennydaon@gmail.com>
2012-10-24 23:37:15 +0200

    Improving search interface
    Making the navbar search work and beutify the search page.
    Only problem so far, is that we've lost the `placehoder` attr
    on the `input` tag.

--- okqa/party/context_processors.py
+++ okqa/party/context_processors.py
@@ -2,7 +2,6 @@ from django.contrib.auth.forms import AuthenticationForm
 from haystack.forms import SearchForm
 
 def forms(request):
-
     context = {"search_form": SearchForm()}
     if not request.user.is_authenticated():
         context["login_form"] = AuthenticationForm()
