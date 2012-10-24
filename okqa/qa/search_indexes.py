ebd011aa50ccec15b219a2cbe9f11985a2423432
Benny Daon <bennydaon@gmail.com>
2012-10-24 22:52:19 +0200

    Adding Answers to the search

--- okqa/qa/search_indexes.py
+++ okqa/qa/search_indexes.py
@@ -2,6 +2,17 @@ import datetime
 from haystack import indexes
 from models import Question, Answer
 
+class AnswerIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    author = indexes.CharField(model_attr='author')
+    created_at = indexes.DateTimeField(model_attr='created_at')
+
+    def get_model(self):
+        return Answer
+
+    def index_queryset(self):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.all()
 
 class QuestionIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
