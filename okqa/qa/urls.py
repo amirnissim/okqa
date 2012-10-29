0bdc9b998fcb10fa3560078c71be26735a8f7999
Paul Walsh <hello@prjts.com>
2012-10-29 22:39:58 +0200

    Sitemaps.

--- okqa/qa/urls.py
+++ okqa/qa/urls.py
@@ -1,20 +1,23 @@
 from django.conf.urls.defaults import patterns, url
+from okqa.qa.sitemaps import sitemaps
 
-urlpatterns = patterns('okqa.qa.views',
-    url(r'^/?$', 'questions', name='home'),
+urlpatterns = patterns('',
+    url(r'^/?$', 'okqa.qa.views.questions', name='home'),
 
-    url(r'^q/(?P<q_id>\d+)/$', 'view_question', name='question-details'),
+    url(r'^q/(?P<q_id>\d+)/$', 'okqa.qa.views.view_question', name='question-details'),
 
-    url(r'^candidates/$', 'candidates', name='candidates'),
+    url(r'^candidates/$', 'okqa.qa.views.candidates', name='candidates'),
     (r'^candidate/(?P<candidate_id>\d+)/$', 'view_candidate'),
 
-    url(r'^members/$', 'members', name='members'),
-    (r'^members/(?P<member_id>\d+)/$', 'view_member'),
+    url(r'^members/$', 'okqa.qa.views.members', name='members'),
+    (r'^members/(?P<member_id>\d+)/$', 'okqa.qa.views.view_member'),
 
-    url(r'^a/post/(?P<q_id>\d+)/$', 'post_answer', name='post_answer'),
-    url(r'^q/post/$', 'post_question', name='post_question'),
-    url(r'^upvote_question/(?P<q_id>\d+)/$', 'upvote_question', name='upvote_question'),
-	
-	url(r'^tags/(?P<tags>.+)/$', 'tagged_questions', name="show_tags"),
+    url(r'^a/post/(?P<q_id>\d+)/$', 'okqa.qa.views.post_answer', name='post_answer'),
+    url(r'^q/post/$', 'okqa.qa.views.post_question', name='post_question'),
+    url(r'^upvote_question/(?P<q_id>\d+)/$', 'okqa.qa.views.upvote_question', name='upvote_question'),
+    url(r'^tags/(?P<tags>.+)/$', 'okqa.qa.views.tagged_questions', name="show_tags"),
+    #(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
 
+    (r'^sitemap\.xml$', 'django.contrib.sitemaps.views.index', {'sitemaps': sitemaps}),
+    (r'^sitemap-(?P<section>.+)\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
 )
