00d62e62e25b2f0867fa9c79b079e28693462666
Benny Daon <bennydaon@gmail.com>
2012-10-31 15:39:27 +0200

    robots.txt are here

--- okqa/urls.py
+++ okqa/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,6 +8,11 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^robots\.txt',
+        TemplateView.as_view(template_name='robots.txt')
+    ),
+
     url(r'', include('okqa.qa.urls')),
     url(r'', include('okqa.user.urls')),
     url(r'accounts/', include('registration.backends.default.urls')),
