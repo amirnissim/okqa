e841833403f6ea63180d2df76b39305349f54e98
Benny Daon <bennydaon@gmail.com>
2012-11-06 10:48:55 +0200

    errrr

--- okqa/party/models.py
+++ okqa/party/models.py
@@ -4,7 +4,6 @@ from django.utils.translation import ugettext as _
 from django.db.models.signals import post_save
 from django.contrib.sites.models import Site
 from django.contrib.auth.models import User
-from okqa.utils import disable_for_loaddata
 
 # Create your models here.
 class Party(models.Model):
