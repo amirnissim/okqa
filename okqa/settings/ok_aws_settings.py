fb38fe580d4a2158a62dd8f7470b582585dbf3a2
Benny Daon <bennydaon@gmail.com>
2012-11-07 15:18:26 +0200

    eerere

--- okqa/settings/ok_aws_settings.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.environ.update({
-    'AWS_ACCESS_KEY_ID': 'AKIAJRMWVZQZITHU73JQ',
-    'AWS_SECRET_ACCESS_KEY': '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',
-    'AWS_STORAGE_BUCKET_NAME': 'ok-qa-media',
-})
-
