a4a24ad0cfdb10cb5a05225b4433bc1047b107fd
Benny Daon <bennydaon@gmail.com>
2012-11-11 11:23:16 +0200

    Adding party name and improving page title

--- okqa/party/models.py
+++ okqa/party/models.py
@@ -9,9 +9,14 @@ from django.contrib.auth.models import User
 class Party(models.Model):
     site = models.ForeignKey(Site)
     logo = models.ImageField(upload_to='logos')
+    name = models.CharField(_("party name"), max_length=30, null=True,
+            help_text =_("In the format: 'The <partyname> Party'"))
     first_in_list = models.ForeignKey(User, null=True, blank=True)
     url = models.URLField(_('Home page'))
     primaries_date = models.DateField(_('Primaries Date'), null=True)
     qualifying_date = models.DateField(_('Qualifying date'), null=True)
     number_of_members = models.IntegerField(_('Number of party members'), null=True)
     open_knesset_id = models.IntegerField(null=True)
+
+    def __unicode__(self):
+        return self.name if self.name else _("anonymous")
