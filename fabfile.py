3b3446a70ea36d0d838035e332b6e8ace5341c3c
Benny Daon <bennydaon@gmail.com>
2012-11-11 13:45:14 +0200

    Fixing the candidate list and the answer truncate

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,13 @@
 from fabric.api import local
 
+def deploy_party(party):
+    local('git push %s master' % party)
+    local('heroku run --app %s python manage.py syncdb --migrate' % party)
+    local('heroku run --app %s python manage.py collectstatic --noinput' % party)
+
 def deploy():
-    local('git push heroku master')
-    local('heroku run python manage.py collectstatic --noinput')
+    for party in ('likud', 'havoda'):
+        deploy_party(party)
 
 def runserver():
     local('python manage.py collectstatic --noinput')
