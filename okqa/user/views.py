8071b404d67a7aab27904296bbbe3d231e565acc
Benny Daon <bennydaon@gmail.com>
2012-11-13 17:05:29 +0200

    Making it simple for an admin to add a candidate. #51

--- okqa/user/views.py
+++ okqa/user/views.py
@@ -8,11 +8,14 @@ from django.contrib.auth.decorators import login_required
 from django.utils.translation import ugettext as _
 from django.contrib import messages
 
+from registration.backends import get_backend
+from registration.models import RegistrationProfile
 from .forms import *
 from .models import *
 
 # TODO: move to settings
 CANDIDATES_GROUP_NAME = "candidates"
+candidate_group = Group.objects.get(name=CANDIDATES_GROUP_NAME)
 def login (request):
     if request.user.is_authenticated():
         return HttpResponseForbidden(_('You are already logged in'))
@@ -33,8 +36,7 @@ def candidate_list(request):
     """
     list candidates ordered by number of answers
     """
-    g = Group.objects.get(name=CANDIDATES_GROUP_NAME)
-    candidates = g.user_set.all().annotate(num_answers=Count('answers')).order_by("-num_answers")
+    candidates = candidate_group.user_set.all().annotate(num_answers=Count('answers')).order_by("-num_answers")
     return render(request, "user/candidate_list.html", {"candidates": candidates})
 
 
@@ -51,7 +53,6 @@ def user_detail(request, slug):
     return render(request, "user/candidate_detail.html", 
             {"candidate": user, "answers": answers, "questions": questions})
 
-
 @login_required
 def edit_profile(request):
     profile = request.user.get_profile()
@@ -66,3 +67,28 @@ def edit_profile(request):
     elif request.method == "GET":
         form = ProfileForm(request.user)
     return render(request, "user/edit_profile.html", {"form": form})
+
+def candidate_activate(request, activation_key):
+    if request.method == "POST":
+        backend = get_backend ('okqa.user.candidate_registration_backend.CandidateBackend')
+        user = backend.activate (request, activation_key)
+        if user:
+            form = ActivateCandidateForm(user, data=request.POST)
+            if form.is_valid():
+                # save the user
+                form.save()
+                user.groups.add(candidate_group)
+                # and log him in
+                user = authenticate(username=user.username, password=form.cleaned_data['new_password1'])
+                auth_login(request, user)
+
+                return HttpResponseRedirect(user.get_absolute_url())
+    elif request.method == "GET":
+        try:
+            profile = RegistrationProfile.objects.get(activation_key=activation_key)
+        except RegistrationProfile.DoesNotExist:
+            return HttpResponseForbidden(_('Sorry, this link has already been used'))
+        form = ActivateCandidateForm(profile.user)
+
+    return render(request, "user/candidate_activate.html", {"form": form})
+
