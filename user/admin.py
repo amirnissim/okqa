9c29f3d509bb12c74122adb60276741126e44fa7
Benny Daon <bennydaon@gmail.com>
2013-06-09 12:30:40 +0300

    DRYing

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,4 @@
+import re
 from django import forms
 from django.contrib import admin
 from django.contrib.auth.models import User
@@ -6,29 +7,25 @@ from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import ugettext as _
 from user.models import Profile
 
+unicode_username = forms.RegexField(label=_("Username"), max_length=30,
+        regex=re.compile(r'^[\w.@+-]{4,}$', re.U),
+        help_text = _("Required. 30 characters or fewer. Alphanumeric \
+characters only (letters, digits and underscores)."),
+        error_message = _("This value must contain only letters, \
+numbers and underscores.")) 
+
 class ProfileAdmin(admin.StackedInline):
     model = Profile
 
 # Overrides django.contrib.auth.forms.UserCreationForm and changes 
 # username to accept a wider range of character in the username. 
 class UserCreationForm(UserCreationForm):
-    username = forms.RegexField(label=_("Username"), max_length=30,
-                regex=r'^(?u)[\w.@+-]{4,}$',
-                help_text = _("Required. 30 characters or fewer. Alphanumeric \
-characters only (letters, digits and underscores)."),
-        error_message = _("This value must contain only letters, \
-numbers and underscores."))
-
+    username = unicode_username
 
 # Overrides django.contrib.auth.forms.UserChangeForm and changes 
 # username to accept a wider range of character in the username. 
 class UserChangeForm(UserChangeForm): 
-    username = forms.RegexField(label=_("Username"), max_length=30,
-        regex=r'^(?u)[\w.@+-]{4,}$',
-        help_text = _("Required. 30 characters or fewer. Alphanumeric \
-characters only (letters, digits and underscores)."),
-        error_message = _("This value must contain only letters, \
-numbers and underscores.")) 
+    username = unicode_username
 
 class UnicodeUserAdmin(UserAdmin):
     form = UserChangeForm
