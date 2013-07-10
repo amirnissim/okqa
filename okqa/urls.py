1d72385aa72e87c3ab9344e61d459dd4ebd1e954
Benny Daon <bennydaon@gmail.com>
2013-07-10 13:24:26 +0300

    Adding a home page with the list of places

--- okqa/urls.py
+++ okqa/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls.defaults import patterns, include, url
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from django.contrib import admin
+from entities.views.ui import EntityDetail, EntityList
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', EntityList.as_view(), name="home"),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
@@ -19,7 +21,6 @@ urlpatterns = patterns('',
     url(r'', include('qa.urls')),
     url(r'', include('user.urls')),
     url(r'', include('social_auth.urls')),
-    url(r'places/', include('entities.urls.ui')),
     (r'^(?P<entity>)/search/', include('search.urls')),
     (r'^taggit_autosuggest/', include('taggit_autosuggest.urls')),
     # flat pages to help with static pages
